5b2d23027eb3eabb435304d76e0f89ae4ddaeda3
Ashaaryaberjimech <138897769+Ashaaryaberjimech@users.noreply.github.com>
2024-01-22 14:47:38 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,5 @@
+def square(number):
+  return number * number
+  user_number = int(input("Enter number:"))
+  squared_number = square(user_number)
+  print(f"{user_number} squared is {squared_number}.")
